5a0d9e2f62787bd2227fe071c54a396eb6b9d4ef
Francois Robino <robino.francois973@gmail.com>
2024-12-22 16:39:40 +0100

    #5 - add model table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_22_153533) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_22_153921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_22_153533) do
     t.string "city_name"
     t.float "latitude"
     t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string "model_designation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
