1c61580327c2d9098dbf90e5acc7ae5c2899acc6
Francois Robino <robino.francois973@gmail.com>
2024-12-22 16:36:29 +0100

    #5 - add brand table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_22_151237) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_22_153533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "brand_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cities", force: :cascade do |t|
     t.string "city_name"
