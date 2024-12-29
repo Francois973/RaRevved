e28c379bcd97edbfc9e3413540e148f456359195
Francois Robino <robino.francois973@gmail.com>
2024-12-29 16:04:58 +0100

    #13 - add first list of adv view

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -2,4 +2,8 @@ class AdvertisementsController < ApplicationController
   def index
     @advertisements = Advertisement.active.includes(:car_product, :user)
   end
+
+  def show
+    @advertisement = Advertisement.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', :as => :rails_health_check
   get '/advertisements', to: 'advertisements#index'
+  get '/advertisements/:id', to: 'advertisements#show'
   # Defines the root path route ("/")
   # root "posts#index"
   # devise_for :users
